fb7578722f5c149436e02d113bbf8e9bc389df55
Quang <quang@gmail.com>
2022-05-28 07:56:08 +0700

    BTL

--- BTL/shop.js
+++ BTL/shop.js
@@ -1,28 +1,28 @@
 const list = [
     {
-        img: './img/item1-1.webp' ,
+        bg: './img/item1-1.webp' ,
         oldPri: '$15.00',
-        newPri: '$13.50$',
+        newPri: '$13.50',
     },
     {
-        img: './img/item2-1.webp',
+        bg: './img/item2-1.webp',
         oldPri: '$15.00',
-        newPri: '$13.50$',
+        newPri: '$13.50',
     },
     {
-        img: './img/item3-1.webp' ,
+        bg: './img/item3-1.webp' ,
         oldPri: '$15.00',
-        newPri: '$13.50$',
+        newPri: '$13.50',
     },
     {
-        img: './img/item4-1.webp' ,
+        bg: './img/item4-1.webp' ,
         oldPri: '$15.00',
-        newPri: '$13.50$',
+        newPri: '$13.50',
     },
     {
-        img: './img/item5-1.webp' ,
+        bg: './img/item5-1.webp' ,
         oldPri: '$15.00',
-        newPri: '$13.50$',
+        newPri: '$13.50',
     },
 ];
 
@@ -31,16 +31,16 @@ const allItem = document.querySelector('#all-item');
 let allItemHtml = '';
 
 list.forEach(function (el) {
-    itemHtml += `<div class="item-container">
-            <div class="item-image" style="background-image: url('${el.img}');">
-            <div class="quick-view">Quick View</div>
-            </div>
-            <p>I'm a product</p>
-            <div class="price">
-                <div class="old-pri">${el.oldPri}</div>
-                <div class="new-pri">${el.newPri}</div>
-            </div>
-            <button class="add">Add to Cart</button>
+    allItemHtml += `<div class="item-cont">
+    <div class="item-image" style="background-image: url('${el.bg}');">
+    <div class="quick-view">Quick View</div>
+    </div>
+    <p>I'm a product</p>
+    <div class="price">
+        <div class="old-pri">${el.newPri}</div>
+        <div class="new-pri">${el.oldPri}</div>
+    </div>
+    <button class="add">Add to Cart</button>
     </div>`;
     
 });
