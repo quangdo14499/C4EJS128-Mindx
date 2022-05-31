4ac02030cdacf747006c32988b980b492406396e
Quang <quang@gmail.com>
2022-05-31 22:39:28 +0700

    bai kiem tra

--- buoi17/bai1.js
+++ buoi17/bai1.js
@@ -1,6 +1,18 @@
+const a = 2
+const b = 5
 
-
-function kiem_tra_so_nguyen_to(n){
-   if (n<2){
-       return
-   }
+function viewStn(){
+    let flag;
+    for (i=a+1;i<=b;i++){
+        flag=1;
+        for(j=2;j<i/2;j++){
+            if(i%j==0){
+                flag=0;
+                break;
+            }
+        }
+        if(flag==1){
+            document.write('so nguyen to',i);
+        }
+    }
+}
